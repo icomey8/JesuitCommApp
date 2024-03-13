1989512548eae89aa3d75a6045cae414bc2af58f
icomey8 <i.willcomey@gmail.com>
2024-03-12 22:08:36 -0700

    started implementing date picker needed for Absence Form
    - dependencies from react native paper, react native paper dates
    `

--- AbsenceForm.js
+++ AbsenceForm.js
@@ -1,13 +1,17 @@
 // imports
 import React from 'react';
 import { useForm, Controller } from 'react-hook-form';
-import { Text, View, StyleSheet, Button, TextInput } from 'react-native';
+import { Text, View, StyleSheet, TextInput } from 'react-native';
 import Constants from 'expo-constants';
-
-
+import { DatePickerModal } from 'react-native-paper-dates';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Button } from 'react-native-paper';
+import { enGB, registerTranslation } from 'react-native-paper-dates'
 
 // absence form
 const AbsenceForm = ({ navigation }) => {
+
+// For Hook Forms
     const { register, setValue, handleSubmit, control, reset, formState: { errors } } = useForm({
         defaultValues: {
           firstName: '',
@@ -28,10 +32,28 @@ const AbsenceForm = ({ navigation }) => {
     console.log('errors', errors);
 
 
+// For Date Picker 
+    const [range, setRange] = React.useState({ startDate: undefined, endDate: undefined });
+    const [open, setOpen] = React.useState(false);
+
+    const onDismiss = React.useCallback(() => {
+        setOpen(false);
+      }, [setOpen]);
+
+    const onConfirm = React.useCallback(
+        ({ startDate, endDate }) => {
+          setOpen(false);
+          setRange({ startDate, endDate });
+        },
+        [setOpen, setRange]
+      );
+    registerTranslation('en', enGB)
+
     
     return (
+        <SafeAreaProvider>
         <View style={styles.container}>
-          <Text style={styles.label}>first Name</Text>
+          <Text style={styles.label}> First Name</Text>
           <Controller
             control={control}
             render={({field: { onChange, onBlur, value }}) => (
@@ -45,7 +67,7 @@ const AbsenceForm = ({ navigation }) => {
             name="firstName"
             rules={{ required: true }}
           />
-          <Text style={styles.label}>first name</Text>
+          <Text style={styles.label}> Last Name</Text>
           <Controller
             control={control}
             render={({field: { onChange, onBlur, value }}) => (
@@ -59,7 +81,39 @@ const AbsenceForm = ({ navigation }) => {
             name="lastName"
             rules={{ required: true }}
           />
-    
+
+
+          <Text style={styles.label}>Purpose of Absence</Text>
+          <Controller
+            control={control}
+            render={({field: { onChange, onBlur, value }}) => (
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={value => onChange(value)}
+                value={value}
+              />
+            )}
+            name="lastName"
+            rules={{ required: true }}
+          />
+
+        <Text style={styles.label}>Date of Absence</Text>
+        <Button onPress={() => setOpen(true)} uppercase={false} mode="outlined">
+          Pick range
+        </Button>
+        <DatePickerModal 
+             disableStatusBarPadding
+             locale='en'
+             visible={open}
+             onDismiss={onDismiss}
+             startDate={range.startDate}
+             endDate={range.endDate}
+             onConfirm={onConfirm}
+             startYear={2023}
+             endYear={2024}
+        />
+
     
           <View style={styles.button}>
             <Button
@@ -70,6 +124,7 @@ const AbsenceForm = ({ navigation }) => {
             />
           </View>
         </View>
+        </SafeAreaProvider>
         )
     };
     // don't forget return to home page button above
---
+++ Absence_Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Absence_Form = () => {
+  return (
+    <div>
+        <form>
+            
+        </form>
+    </div>
+  )
+}
